e25289c1ff21b5892dd62d8babd167bdad043861
Mohammad Javad <126692128+hejazij88@users.noreply.github.com>
2024-04-04 12:17:35 +0330

    Add files via upload

---
+++ Expression Visitor/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Linq.Expressions;
+
+List<Person> list = new List<Person>
+{
+    new Person{Id = 1,Name = "Mohammad Javad"},
+    new Person{Id = 2,Name = "Ali"},
+    new Person{Id = 3,Name = "Mohsen"},
+};
+
+
+IQueryable<Person> lists = list.AsQueryable();
+IQueryable<Person> query = lists.Where(p=>p.Id<=2)
+    .OrderBy(p=>p.Name);
+
+
+QueryVisitor visitor = new QueryVisitor();
+visitor.Visit(query.Expression);
+
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class QueryVisitor : ExpressionVisitor
+{
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        return base.VisitBinary(node);
+    }
+
+    protected override Expression VisitConstant(ConstantExpression node)
+    {
+        return base.VisitConstant(node);
+    }
+
+    protected override Expression VisitMember(MemberExpression node)
+    {
+        return base.VisitMember(node);
+    }
+}
